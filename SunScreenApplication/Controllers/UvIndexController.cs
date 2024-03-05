8c3f7c30ee2cf420bf322ce0f6147f870a4564fe
rantad584 <rantad584@github.com>
2024-03-05 22:32:40 +1100

    US1.1 Set up postcode form and extract location data from database

---
+++ SunScreenApplication/Controllers/UvIndexController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SunScreenApplication.Models;
+
+namespace SunScreenApplication.Controllers
+{
+    public class UvIndexController : Controller
+    {
+        private LocationDBContext _db;
+
+        public UvIndexController(LocationDBContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Form()
+        {
+            return View();
+        }
+
+        public IActionResult Results()
+        {
+            return View();
+        }
+
+        // POST: /UvIndex/Form
+        [HttpPost]
+        public IActionResult Form(string postcode)
+        {
+            var location = _db.postcodes_geo.FirstOrDefault(l => l.Postcode == postcode);
+
+            if (location != null)
+            {
+                // Redirect to another action method
+                return RedirectToAction("Results", new { latitude = location.Latitude, longitude = location.Longitude });
+            }
+            else
+            {
+                // Handle the case where no data is found for the provided postcode
+                return RedirectToAction("Form");
+            }
+        }
+    }
+}
---
+++ SunScreenApplication/Models/Location.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace SunScreenApplication.Models
+{
+    public class Location
+    {
+        public int Id { get; set; }
+        public string? Postcode { get; set; }
+        public string? Suburb { get; set; }
+        public string? State { get; set; }
+        public decimal Latitude { get; set; }
+        public decimal Longitude { get; set; }
+    }
+
+    public class LocationDBContext : DbContext
+    {
+        public LocationDBContext(DbContextOptions<LocationDBContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Location> postcodes_geo { get; set; }
+    }
+}
